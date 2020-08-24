a6f7ca9c3b9472dc73d1c97d9f3cc9799b08b7d7
itsadeadh2 <itsadeadh2@gmail.com>
2020-08-24 01:49:31 -0400

    feat: remove sensitive data

--- config/envs/variables.js
+++ config/envs/variables.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: process.env.APP_PORT,
   waitTimeHubspot: process.env.APP_HUBSPOT_DELAY || 500,
-  apiKey: process.env.APP_API_KEY || "ff8e0385-1e87-4661-ba5d-b25e16fd67a0",
+  apiKey: process.env.APP_API_KEY,
   nomeCandidato: process.env.APP_NOME_CAND || "Thiago",
   sobreNomeCandidato: process.env.APP_SOBREN_CAND || "Barbosa da Silva",
 };
